3f29227591a2f9b25c7338b23a706cc6714edbf4
Felix <felixdenatris@hotmail.nl>
2020-07-06 11:11:21 +0200

    clean up

--- src/mge/config.hpp
+++ src/mge/config.hpp
@@ -1,7 +1,7 @@
 #include <string>
 namespace config {
-    const std::string MGE_MODEL_PATH ("C:/Users/Felix/Documenten/Saxion/Advanced_Tools/mge_v19_student_version/assets/mge/models/");
-    const std::string MGE_TEXTURE_PATH ("C:/Users/Felix/Documenten/Saxion/Advanced_Tools/mge_v19_student_version/assets/mge/textures/");
-    const std::string MGE_SHADER_PATH ("C:/Users/Felix/Documenten/Saxion/Advanced_Tools/mge_v19_student_version/assets/mge/shaders/");
-    const std::string MGE_FONT_PATH ("C:/Users/Felix/Documenten/Saxion/Advanced_Tools/mge_v19_student_version/assets/mge/fonts/");
+    const std::string MGE_MODEL_PATH ("mge/models/");
+    const std::string MGE_TEXTURE_PATH ("mge/textures/");
+    const std::string MGE_SHADER_PATH ("mge/shaders/");
+    const std::string MGE_FONT_PATH ("mge/fonts/");
 }
